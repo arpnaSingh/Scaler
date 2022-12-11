0b375391cda600f1874c7a4cd88c7fa054ee61f2
arpnasingh <arpnasingh89@gmail.com>
2022-12-11 13:05:04 +0530

    IsPerfectNumber

--- Introduction to problem solving/Program.cs
+++ Introduction to problem solving/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Assignment
 Console.WriteLine("CountFactors: " + CountFactors.GetFactors(27));
 Console.WriteLine("PrimeNubmers: " + PrimeNumber.IsPrimeNumer(27));
 Console.WriteLine("SquareRoot: " + SquareRoot.GetSquareRoot(27));
 Console.WriteLine("Iterations: " + Iterations.FindIterationsForSqrt(9));
 Console.WriteLine("SumOfNNumbers: " + SumOfNNumbers.GetSum(49));
+#endregion Assignment
+
+#region Homerwork
+Console.WriteLine("PerfectNumber: " + PerfectNumber.IsPerfectNumber(27));
+#endregion Homerwork
