51b04548a21303714d6aa6be363d4b4d9ff54f61
arpnasingh <arpnasingh89@gmail.com>
2022-12-11 12:46:41 +0530

    Iteraions

--- Introduction to problem solving/Program.cs
+++ Introduction to problem solving/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
- Console.WriteLine("CountFactors: "+CountFactors.GetFactors(27));
- Console.WriteLine("PrimeNubmers: "+PrimeNumber.IsPrimeNumer(27));
- Console.WriteLine("SquareRoot: "+SquareRoot.GetSquareRoot(27));
+Console.WriteLine("CountFactors: " + CountFactors.GetFactors(27));
+Console.WriteLine("PrimeNubmers: " + PrimeNumber.IsPrimeNumer(27));
+Console.WriteLine("SquareRoot: " + SquareRoot.GetSquareRoot(27));
+Console.WriteLine("Iterations: " + Iterations.FindIterationsForSqrt(9));
