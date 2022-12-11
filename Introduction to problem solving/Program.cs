488cacf2d771f7a1fb266d524ff01b0e85d85f63
arpnasingh <arpnasingh89@gmail.com>
2022-12-11 12:54:39 +0530

    SumOfNNumbers

--- Introduction to problem solving/Program.cs
+++ Introduction to problem solving/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("CountFactors: " + CountFactors.GetFactors(27));
 Console.WriteLine("PrimeNubmers: " + PrimeNumber.IsPrimeNumer(27));
 Console.WriteLine("SquareRoot: " + SquareRoot.GetSquareRoot(27));
 Console.WriteLine("Iterations: " + Iterations.FindIterationsForSqrt(9));
+Console.WriteLine("SumOfNNumbers: " + SumOfNNumbers.GetSum(49));
