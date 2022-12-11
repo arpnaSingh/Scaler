9251a6f5b31ee95a5e1ece7564872a020d2e8146
arpnasingh <arpnasingh89@gmail.com>
2022-12-11 12:37:54 +0530

    isPrime number

--- Introduction to problem solving/Program.cs
+++ Introduction to problem solving/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
  Console.WriteLine("CountFactors: "+CountFactors.GetFactors(27));
+ Console.WriteLine("PrimeNubmers: "+PrimeNumber.IsPrimeNumer(27));
